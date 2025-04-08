cd37f3d262347532ae587f53ad601f943308767c
Kose117 <josemrodriguez@javeriana.edu.co>
2025-04-08 17:09:55 -0500

    Implement code changes to enhance functionality and improve performance

--- src/components/templates/AuthTemplate.tsx
+++ src/components/templates/AuthTemplate.tsx
@@ -1,7 +1,7 @@
 import AuthForm from "@/components/organisms/Auth-form"
 import { FormField } from "@/types/formTypes"
 import { LoginInput, User } from "../../types"
-import fondoFrutal from "../../assets/Fondo.jpg"
+import fondoFrutal from "../../assets/Fondo1.jpg"
 
 interface AuthTemplateProps {
   loginFields: FormField[]
@@ -18,10 +18,11 @@ export default function AuthTemplate({
 }: AuthTemplateProps) {
   return (
     <main
-      className="flex items-center justify-center h-screen p-4 overflow-x-auto"
+      className="grid h-screen w-screen p-8 gap-8 overflow-x-hidden grid-cols-1 xl:grid-cols-2 "
       style={{ backgroundImage: `url(${fondoFrutal})`, backgroundSize: "cover", backgroundPosition: "center" }}
     >
-      <section className="w-full max-w-md p-6 bg-white rounded-lg shadow-md">
+      <div></div>
+      <section className=" h-fit max-w-md p-6 bg-white rounded-lg shadow-md justify-self-center w-1/2 overflow-x-auto self-center">
       <AuthForm
         loginFields={loginFields}
         registryFields={registryFields}
